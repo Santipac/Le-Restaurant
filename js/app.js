9f3358e2673e6a9c595dd16f92f1369379c78bcb
Santiago <santiagopacini@gmail.com>
2022-05-12 14:42:05 -0300

    adding a scroll's event for header, a contact form and starting with footer

--- js/app.js
+++ js/app.js
@@ -1,40 +1,48 @@
-const toggle = document.querySelector('.toggle')
-const close = document.querySelector('.close')
-const nav = document.querySelector('nav')
+const toggle = document.querySelector(".toggle");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
 
-    toggle.addEventListener('click', () => {
-        nav.classList.add('open-nav')
-    })
 
-    close.addEventListener('click', () => {
-        nav.classList.remove('open-nav')
-    })
+//NAVBAR TOGGLE
+toggle.addEventListener("click", () => {
+    nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+    nav.classList.remove("open-nav");
+});
+
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolling-active', window.scrollY > 500 );
+})
 
 
 window.sr = ScrollReveal();
 
-    /*sr.reveal('.header', {
+/*sr.reveal('.header', {
         duration: 2000,
         origin: 'bottom',
         distance: '-100px'
     });*/
-    sr.reveal('.about-text', {
-        duration: 2000,
-        origin: 'bottom',
-        distance: '-100px'
-    });
-    sr.reveal('.about-img', {
-        duration: 2000,
-        origin: 'bottom',
-        distance: '-100px'
-    });
-    sr.reveal('.banner', {
-        duration: 1000,
-        origin: 'left',
-        distance: '100%'
-    });
-    sr.reveal('.food__row', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100%'
-    });
+sr.reveal(".about-text", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "-100px",
+});
+sr.reveal(".about-img", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "-100px",
+});
+sr.reveal(".banner", {
+    duration: 1000,
+    origin: "left",
+    distance: "100%",
+});
+sr.reveal(".food__row", {
+    duration: 2000,
+    origin: "top",
+    distance: "-100%",
+});
